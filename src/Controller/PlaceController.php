45d8440f00f369dae84c21152ffa20a1c0140367
yassine0803 <aourhebal.yassine@gmail.com>
2020-06-02 17:37:19 +0100

    fos_rest.view_handler configured in PlaceController

--- src/Controller/PlaceController.php
+++ src/Controller/PlaceController.php
@@ -6,35 +6,48 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\Annotations\Get;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\View\ViewHandler;
+use FOS\RestBundle\View\View;
 use App\Entity\Place;
 
 class PlaceController extends Controller
 {
 
     /**
-     * @Get("/places")
+     * @Rest\View()
+     * @Rest\Get("/places")
      */
     public function getPlacesAction(Request $request)
     {
         $places = $this->get('doctrine.orm.entity_manager')
-            ->getRepository(Place::class)
-            ->findAll();
+                ->getRepository(Place::class)
+                ->findAll();
         /* @var $places Place[] */
 
         $formatted = [];
         foreach ($places as $place) {
             $formatted[] = [
-                'id' => $place->getId(),
-                'name' => $place->getName(),
-                'address' => $place->getAddress(),
+               'id' => $place->getId(),
+               'name' => $place->getName(),
+               'address' => $place->getAddress(),
             ];
         }
 
-        return new JsonResponse($formatted);
+         // Récupération du view handler
+        $viewHandler = $this->get('fos_rest.view_handler');
+
+        // Création d'une vue FOSRestBundle
+        $view = View::create($formatted);
+        $view->setFormat('json');
+
+        // Gestion de la réponse
+        return $viewHandler->handle($view);
     }
+    
 
     /**
      * @Get("/places/{id}")
