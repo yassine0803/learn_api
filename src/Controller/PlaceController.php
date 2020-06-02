49236cef44021d45d96bca4e48aba600e0626b60
yassine0803 <aourhebal.yassine@gmail.com>
2020-06-02 15:47:52 +0100

    route of users configured with FOSRestBundle

--- src/Controller/PlaceController.php
+++ src/Controller/PlaceController.php
@@ -1,10 +1,12 @@
 <?php
+
 namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use FOS\RestBundle\Controller\Annotations\Get;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Place;
@@ -12,38 +14,44 @@ use App\Entity\Place;
 class PlaceController extends Controller
 {
 
+    /**
+     * @Get("/places")
+     */
     public function getPlacesAction(Request $request)
     {
         $places = $this->get('doctrine.orm.entity_manager')
-                ->getRepository(Place::class)
-                ->findAll();
+            ->getRepository(Place::class)
+            ->findAll();
         /* @var $places Place[] */
 
         $formatted = [];
         foreach ($places as $place) {
             $formatted[] = [
-               'id' => $place->getId(),
-               'name' => $place->getName(),
-               'address' => $place->getAddress(),
+                'id' => $place->getId(),
+                'name' => $place->getName(),
+                'address' => $place->getAddress(),
             ];
         }
 
         return new JsonResponse($formatted);
     }
 
+    /**
+     * @Get("/places/{id}")
+     */
     public function getPlaceAction($id, Request $request)
     {
         $place = $this->get('doctrine.orm.entity_manager')
-                ->getRepository(Place::class)
-                ->find($id);
+            ->getRepository(Place::class)
+            ->find($id);
         /* @var $place Place */
         if (empty($place)) {
             return new JsonResponse(['message' => 'Place not found'], Response::HTTP_NOT_FOUND);
         }
         $formatted = [
-           'id' => $place->getId(),
-           'name' => $place->getName(),
-           'address' => $place->getAddress(),
+            'id' => $place->getId(),
+            'name' => $place->getName(),
+            'address' => $place->getAddress(),
         ];
 
         return new JsonResponse($formatted);
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,10 +1,12 @@
 <?php
+
 namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use FOS\RestBundle\Controller\Annotations\Get;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\User;
@@ -12,22 +14,22 @@ use App\Entity\User;
 class UserController extends Controller
 {
     /**
-     * @Route("/users", name="users_list")
+     * @Get("/users")
      */
     public function getUsersAction(Request $request)
     {
         $users = $this->get('doctrine.orm.entity_manager')
-                ->getRepository(User::class)
-                ->findAll();
+            ->getRepository(User::class)
+            ->findAll();
         /* @var $users User[] */
 
         $formatted = [];
         foreach ($users as $user) {
             $formatted[] = [
-               'id' => $user->getId(),
-               'firstname' => $user->getFirstname(),
-               'lastname' => $user->getLastname(),
-               'email' => $user->getEmail(),
+                'id' => $user->getId(),
+                'firstname' => $user->getFirstname(),
+                'lastname' => $user->getLastname(),
+                'email' => $user->getEmail(),
             ];
         }
 
@@ -35,22 +37,22 @@ class UserController extends Controller
     }
 
     /**
-     * @Route("/users/{id}", name="users_one")
+     * @Get("/users/{user_id}")
      */
     public function getUserAction(Request $request)
     {
         $user = $this->get('doctrine.orm.entity_manager')
-                ->getRepository(User::class)
-                ->find($request->get('id'));
+            ->getRepository(User::class)
+            ->find($request->get('id'));
         /* @var $user User */
         if (empty($place)) {
             return new JsonResponse(['message' => 'Place not found'], Response::HTTP_NOT_FOUND);
         }
         $formatted = [
-           'id' => $user->getId(),
-           'firstname' => $user->getFirstname(),
-           'lastname' => $user->getLastname(),
-           'email' => $user->getEmail(),
+            'id' => $user->getId(),
+            'firstname' => $user->getFirstname(),
+            'lastname' => $user->getLastname(),
+            'email' => $user->getEmail(),
         ];
 
         return new JsonResponse($formatted);
