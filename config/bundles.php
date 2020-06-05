7c4f747d76d06a03e19e9217c404cd949d1fe064
yassine0803 <aourhebal.yassine@gmail.com>
2020-06-05 11:50:45 +0100

    NelmioApiDocBundle installed

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
